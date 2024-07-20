9757119c494a16942c5f066402ca74cc7df4becc
RivierGrullon <grullonrivier@gmail.com>
2024-07-20 00:56:34 -0400

    feat: Add function to find multiple Steam paths

--- backend/steam/api.ts
+++ backend/steam/api.ts
@@ -2,15 +2,15 @@ import { Context } from "jsr:@oak/oak";
 import { getGamesSteam } from "./mod.ts";
 
 export async function GetGamesSteam({ response, request }: Context) {
-    try {
-        const forced = request.url.searchParams.get("force") === "true";
-
-        response.status = 200;
-        response.body = await getGamesSteam(forced);
-    } catch (error) {
-        response.status = 500;
-        response.body = {
-            error,
-        };
-    }
+  try {
+    const forced = request.url.searchParams.get("force") === "true";
+    // console.log(await getAllDisks());
+    response.status = 200;
+    response.body = await getGamesSteam(forced);
+  } catch (error) {
+    response.status = 500;
+    response.body = {
+      error,
+    };
+  }
 }
--- backend/steam/mod.ts
+++ backend/steam/mod.ts
@@ -1,125 +1,144 @@
 import { walk } from "jsr:@std/fs@0.223/walk";
 import { storage } from "../local-storage.ts";
-
-const steamPath = "C:\\Program Files (x86)\\Steam\\steamapps";
+import { getAllDisks } from "../os/mod.ts";
 
 interface SteamGamesInfo {
-    appid: string;
-    name: string;
-    SizeOnDisk: string;
-    LastPlayed: string;
-    verticalGrids: any;
-    gameInfo: any;
+  appid: string;
+  name: string;
+  SizeOnDisk: string;
+  LastPlayed: string;
+  verticalGrids: any;
+  gameInfo: any;
 }
 
 export const SteamKeys = {
-    games: "steam.data.game",
+  games: "steam.data.game",
 };
 
 const parse = JSON.parse;
 const strinify = JSON.stringify;
 
+async function findSteamPaths(): Promise<string[]> {
+  const disks = await getAllDisks();
+  const pathsToCheck = disks.flatMap((disk) => [
+    `${disk.DeviceID}\\Program Files (x86)\\Steam\\steamapps`,
+    `${disk.DeviceID}\\SteamLibrary\\steamapps`,
+    `${disk.DeviceID}\\Steam\\steamapps`,
+  ]);
+
+  const checkPathPromises = pathsToCheck.map(async (path) => {
+    const steamExists = await Deno.stat(path)
+      .then(() => true)
+      .catch(() => false);
+    return steamExists ? path : null;
+  });
+
+  const resolvedPaths = await Promise.all(checkPathPromises);
+  return resolvedPaths.filter((path) => path !== null) as string[];
+}
+
 export async function getGamesSteam(force = false) {
-    if (force || storage.getItem(SteamKeys.games) === null) {
-        const games: SteamGamesInfo[] = [];
+  if (force || storage.getItem(SteamKeys.games) === null) {
+    const games: SteamGamesInfo[] = [];
+    const steamPaths = await findSteamPaths();
 
-        for await (
-            const file of walk(steamPath, {
-                exts: ["acf"],
-                maxDepth: 1,
-            })
-        ) {
-            if (file.isFile) {
-                const raw = await Deno.readTextFile(file.path);
+    for (const steamPath of steamPaths) {
+      for await (const file of walk(steamPath, {
+        exts: ["acf"],
+        maxDepth: 1,
+      })) {
+        if (file.isFile) {
+          const raw = await Deno.readTextFile(file.path);
 
-                const {
-                    AppState: { appid, name, SizeOnDisk, LastPlayed },
-                } = parseACF(raw) ?? {};
+          const {
+            AppState: { appid, name, SizeOnDisk, LastPlayed },
+          } = parseACF(raw) ?? {};
 
-                games.push({
-                    appid,
-                    name,
-                    SizeOnDisk,
-                    LastPlayed,
-                    verticalGrids: await verticalGrids(appid),
-                    gameInfo: await gameInfo(appid),
-                });
-            }
+          games.push({
+            appid,
+            name,
+            SizeOnDisk,
+            LastPlayed,
+            verticalGrids: await verticalGrids(appid),
+            gameInfo: await gameInfo(appid),
+          });
         }
-
-        storage.setItem(SteamKeys.games, strinify(games));
-
-        return games;
+      }
     }
 
-    const output = storage.getItem(SteamKeys.games);
-    const games: SteamGamesInfo[] = parse(output!);
+    storage.setItem(SteamKeys.games, strinify(games));
 
     return games;
+  }
+
+  const output = storage.getItem(SteamKeys.games);
+  const games: SteamGamesInfo[] = parse(output!);
+
+  return games;
 }
 
 function parseACF(input: string) {
-    const stack: Array<Record<string, any>> = [{}];
-    let currentKey = "";
+  const stack: Array<Record<string, any>> = [{}];
+  let currentKey = "";
 
-    const lines = input.split("\n");
+  const lines = input.split("\n");
 
-    for (const line of lines) {
-        const trimmedLine = line.trim();
-        if (trimmedLine === "{") {
-            const newObj = {};
-            stack[stack.length - 1][currentKey] = newObj;
-            stack.push(newObj);
-        } else if (trimmedLine === "}") {
-            stack.pop();
-        } else if (trimmedLine) {
-            const [key, ...valueParts] = trimmedLine.split("\t\t");
-            const value = valueParts.join("\t\t").replace(/^"/, "").replaceAll(
-                /"$/g,
-                "",
-            );
-            if (value) {
-                stack[stack.length - 1][
-                    key.replaceAll("'", "").replaceAll('"', "")
-                ] = value;
-            } else {
-                currentKey = key.replaceAll("'", "").replaceAll('"', "");
-            }
-        }
+  for (const line of lines) {
+    const trimmedLine = line.trim();
+    if (trimmedLine === "{") {
+      const newObj = {};
+      stack[stack.length - 1][currentKey] = newObj;
+      stack.push(newObj);
+    } else if (trimmedLine === "}") {
+      stack.pop();
+    } else if (trimmedLine) {
+      const [key, ...valueParts] = trimmedLine.split("\t\t");
+      const value = valueParts
+        .join("\t\t")
+        .replace(/^"/, "")
+        .replaceAll(/"$/g, "");
+      if (value) {
+        stack[stack.length - 1][key.replaceAll("'", "").replaceAll('"', "")] =
+          value;
+      } else {
+        currentKey = key.replaceAll("'", "").replaceAll('"', "");
+      }
     }
+  }
 
-    return stack[0];
+  return stack[0];
 }
 
 async function verticalGrids(id: string) {
-    const response = fetch(
-        "https://www.steamgriddb.com/api/v2/grids/steam/" + id +
-            "?limit=50&dimension=['600x900']",
-        {
-            method: "GET",
-            headers: {
-                "Authorization": "Bearer 38799058f978733279591eb51916c358",
-            },
-        },
-    );
+  const response = await fetch(
+    "https://www.steamgriddb.com/api/v2/grids/steam/" +
+      id +
+      "?limit=50&dimension=['600x900']",
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer 38799058f978733279591eb51916c358",
+      },
+    }
+  );
 
-    const data = (await response).json();
+  const data = await response.json();
 
-    return data;
+  return data;
 }
 
 async function gameInfo(id: string) {
-    const response = fetch(
-        "https://www.steamgriddb.com/api/v2/games/steam/" + id,
-        {
-            method: "GET",
-            headers: {
-                "Authorization": "Bearer 38799058f978733279591eb51916c358",
-            },
-        },
-    );
+  const response = await fetch(
+    "https://www.steamgriddb.com/api/v2/games/steam/" + id,
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer 38799058f978733279591eb51916c358",
+      },
+    }
+  );
 
-    const data = (await response).json();
+  const data = await response.json();
 
-    return data;
+  return data;
 }
